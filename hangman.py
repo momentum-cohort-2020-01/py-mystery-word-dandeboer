68a3645a369b72e04b9e1c0e44d7a7ff227d7e9d
dandeboer <ddeboer926@gmail.com>
2020-02-23 23:09:40 -0500

    can no longer lose guesses if player makes a repeat of the same guess. can no longer enter in non letters. uppercase letters are now valid characters.

--- hangman.py
+++ hangman.py
@@ -17,9 +17,6 @@ class Game:
     def difficulty(self):
         pass
 
-    def reset_game(self):
-        pass
-
 
 class Mechanics:
     def __init__(self, f):
@@ -28,43 +25,47 @@ class Mechanics:
         self.char_two = "_"
         self.char_three = "_"
         self.char_four = "_"
-        self.guesses = 6
+        self.guesses = 8
+        self.used_chars = []
+        self.acceptable_chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     def four(self):
         print(f"{self.char_one} {self.char_two} {self.char_three} {self.char_four}")
-        self.response = input(f"You have {self.guesses} guesses. Enter a key to guess ")
+        self.response = input(f"You have {self.guesses} guesses. Enter a key to guess ").lower()
         self.test()
         
     def test(self):
-        # for char in self.f[0:]:
-        if (self.response == self.f[0]):
-            self.char_one = self.response
-        if (self.response == self.f[1]):
-            self.char_two = self.response
-        if (self.response == self.f[2]):
-            self.char_three = self.response
-        if (self.response == self.f[3]):
-            self.char_four = self.response
-
-        if (self.response != self.f[0]) and (self.response != self.f[1]) and (self.response != self.f[2]) and (self.response != self.f[3]):
-            self.guesses -= 1
-            if (self.guesses <= 0):
-                print(f"You lose! the word was {self.f}")
-            else:
-                print("Incorrect \n")
-                self.four()
+        if self.response not in self.acceptable_chars:
+            print("Please use a letter of the alphabet")
+            self.four()
+        if self.response in self.used_chars:
+            print("You have already used that character\n")
+            self.four()
         else:
-            if (self.char_one != "_") and (self.char_two != "_") and (self.char_three != "_") and (self.char_four != "_"):
-                print(f"The word was {self.f} \nYou Win!")
+            self.used_chars.append(self.response)
+
+            if (self.response == self.f[0]):
+                self.char_one = self.response
+            if (self.response == self.f[1]):
+                self.char_two = self.response
+            if (self.response == self.f[2]):
+                self.char_three = self.response
+            if (self.response == self.f[3]):
+                self.char_four = self.response
+
+            if (self.response != self.f[0]) and (self.response != self.f[1]) and (self.response != self.f[2]) and (self.response != self.f[3]):
+                self.guesses -= 1
+                if (self.guesses <= 0):
+                    print(f"You lose! the word was {self.f}")
+                else:
+                    print("Incorrect \n")
+                    self.four()
             else:
-                print("")
-                self.four()
-
-        # self.four()
+                if (self.char_one != "_") and (self.char_two != "_") and (self.char_three != "_") and (self.char_four != "_"):
+                    print(f"The word was {self.f} \nYou Win!")
+                else:
+                    print("")
+                    self.four()
         
-        
 
-            
-
-# print(random.randint(1,26))
 Game().start_game()

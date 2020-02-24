d4370ffa31778d3e667c6ca938fb19a939d3fa26
dandeboer <ddeboer926@gmail.com>
2020-02-23 21:14:13 -0500

    have a game that functions when given a four letter string

--- hangman.py
+++ hangman.py
@@ -19,15 +19,43 @@ class Game:
 class Mechanics:
     def __init__(self, f, x):
         self.f = f[x]
-        self.one = "_"
+        self.char_one = "_"
+        self.char_two = "_"
+        self.char_three = "_"
+        self.char_four = "_"
+        self.guesses = 6
 
     def four(self):
-        print(f"{self.one}")
-        self.response = input("Enter a key to guess ")
+        print(f"{self.char_one} {self.char_two} {self.char_three} {self.char_four}")
+        self.response = input(f"You have {self.guesses} guesses. Enter a key to guess ")
         self.test()
         
     def test(self):
-        if (self.response == self.f[0]) or (self.response == self.f[1]) or (self.response == self.f[2]) or (self.response == self.f[3]):
+        # for char in self.f[0:]:
+        if (self.response == self.f[0]):
+            self.char_one = self.response
+        if (self.response == self.f[1]):
+            self.char_two = self.response
+        if (self.response == self.f[2]):
+            self.char_three = self.response
+        if (self.response == self.f[3]):
+            self.char_four = self.response
+
+        if (self.response != self.f[0]) and (self.response != self.f[1]) and (self.response != self.f[2]) and (self.response != self.f[3]):
+            self.guesses -= 1
+            if (self.guesses <= 0):
+                print(f"You lose! the word was {self.f}")
+            else:
+                print("Incorrect \n")
+                self.four()
+        else:
+            if (self.char_one != "_") and (self.char_two != "_") and (self.char_three != "_") and (self.char_four != "_"):
+                print(f"The word was {self.f} \nYou Win!")
+            else:
+                print("")
+                self.four()
+
+        # self.four()
         
         
 
